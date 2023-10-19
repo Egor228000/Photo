820b1050c35fc58055a7acdd631f75b37c47dbb1
Egor228000 <egorkrasnov123456789@gmail.com>
2023-10-19 14:43:14 +0700

    о

--- app/src/main/java/com/example/nastya_app/Image.kt
+++ app/src/main/java/com/example/nastya_app/Image.kt
@@ -4,8 +4,12 @@ import androidx.compose.animation.core.Animatable
 import androidx.compose.foundation.background
 import androidx.compose.foundation.gestures.detectTapGestures
 import androidx.compose.foundation.gestures.detectTransformGestures
+import androidx.compose.foundation.gestures.rememberTransformableState
+import androidx.compose.foundation.gestures.transformable
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.BoxWithConstraints
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -17,11 +21,16 @@ import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.material.Icon
 import androidx.compose.material.IconButton
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableFloatStateOf
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.scale
+import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.input.pointer.pointerInput
@@ -36,26 +45,32 @@ import kotlinx.coroutines.launch
 
 @Composable
 fun Image(navController: NavController, kartinka: String){
-    val initialScale = 1f
-    val scale = remember { Animatable(initialScale) }
-
-    val coroutineScope = rememberCoroutineScope()
-    Box(
+    BoxWithConstraints(
         Modifier
-        .fillMaxSize()
         .background(Color.White)
-        .pointerInput(Unit) {
-            detectTransformGestures { _, pan, gestureZoom, _ ->
-                coroutineScope.launch {
-                    when {
-                        gestureZoom != 1f -> scale.animateTo(scale.value * gestureZoom)
-                        pan.x != 0f || pan.y != 0f -> {
-                            // Handle panning/dragging if needed
-                        }
-                    }
-                }
-            }
-        }) {
+                .fillMaxWidth()
+          ) {
+        var scale by remember {
+            mutableFloatStateOf(1f)
+        }
+        var offset by remember {
+            mutableStateOf(Offset.Zero)
+        }
+
+        val state = rememberTransformableState { zoomChange, panChange, rotationChange ->
+            scale = (scale * zoomChange).coerceIn(1f, 9f)
+
+            val extraWidth = (scale - 1) * constraints.maxWidth
+            val extraHeight = (scale - 1) * constraints.maxHeight
+
+            val maxX = extraWidth / 3
+            val maxY = extraHeight / 3
+
+            offset = Offset(
+                x = (offset.x + scale * panChange.x).coerceIn(-maxX, maxX),
+                y = (offset.y + scale * panChange.y).coerceIn(-maxY, maxY),
+            )
+        }
 
         SubcomposeAsyncImage(
             model = kartinka,
@@ -63,14 +78,14 @@ fun Image(navController: NavController, kartinka: String){
             contentScale = ContentScale.Fit,
             modifier = Modifier
                 .fillMaxWidth(1f)
-                .fillMaxHeight(1f).graphicsLayer(scaleX = scale.value, scaleY = scale.value)
-                .pointerInput(Unit) {
-                    detectTapGestures {
-                        coroutineScope.launch {
-                            scale.animateTo(initialScale)
-                        }
-                    }
+                .fillMaxHeight(1f)
+                .graphicsLayer {
+                    scaleX = scale
+                    scaleY = scale
+                    translationX = offset.x
+                    translationY = offset.y
                 }
+                .transformable(state)
 
         ) {
 
--- app/src/main/java/com/example/nastya_app/Plus.kt
+++ app/src/main/java/com/example/nastya_app/Plus.kt
@@ -1,13 +1,22 @@
 package com.example.nastya_app
 
+import android.net.Uri
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.animation.Animatable
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.combinedClickable
 import androidx.compose.foundation.gestures.detectTapGestures
 import androidx.compose.foundation.gestures.detectTransformGestures
+import androidx.compose.foundation.gestures.rememberTransformableState
+import androidx.compose.foundation.gestures.transformable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.BoxWithConstraints
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
@@ -19,13 +28,18 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Button
 import androidx.compose.material.Card
 import androidx.compose.material.CircularProgressIndicator
+import androidx.compose.material.Icon
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Menu
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableFloatStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
@@ -33,6 +47,8 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.ImageBitmap
 import androidx.compose.ui.graphics.graphicsLayer
@@ -43,6 +59,7 @@ import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalHapticFeedback
 import androidx.compose.ui.res.imageResource
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.IntOffset
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Dialog
@@ -64,10 +81,6 @@ import kotlin.math.roundToInt
 
 @Composable
 fun Plus(navController: NavController) {
-
-
-
-
     Column(
         Modifier
             .fillMaxSize()
@@ -75,10 +88,7 @@ fun Plus(navController: NavController) {
     ) {
 
 
-
+        }
     }
-}
-
-
 
 
